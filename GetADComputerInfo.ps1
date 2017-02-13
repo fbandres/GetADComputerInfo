310d5b1634f148bf696130878f45bd19cedd9bd5
Francis Paulo Andres <fbandres17@gmail.com>
2017-02-13 23:38:21 +0800

    Add files via upload

---
+++ GetADComputerInfo.ps1
@@ -0,0 +1,14 @@
+﻿##############################################################################################################
+#
+# Title: GetADComputerInfo
+# Author: Francis Paulo B. Andres
+# Website: http://francisandres.com
+# Description: Get Active Directory Computer Information from a CSV file.
+#
+##############################################################################################################
+
+# Import the CSV file to a variable
+$computers = Import-CSV "C:\Users\francis\Desktop\ADComputers.csv"
+
+# Get computer information using Get-ADComputer cmdlet
+$computers | foreach-object {Get-AdComputer -Identity ($_.Servername) | Select Name, Enabled} | Format-Table -Autosize
